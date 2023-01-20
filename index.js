7e816461e85139a74e2af01c7e793e0c5cf923f3
Calu Marquez <carlamarquez.distextil@gmail.com>
2023-01-20 19:36:12 -0300

    fin

--- index.js
+++ index.js
@@ -47,3 +47,26 @@ if(distancia>=300){
 window.onscroll = function(){
     efectoHabilidades();
 }
+
+
+//Enviar e-mail con emailJS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_qzrmyed';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Submitted successfully');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert("There was an error in the email sent");
+    });
+});
